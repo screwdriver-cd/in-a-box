010407bcd5787b19dc039da63a1c0f4d84cbe750
Tiffany K <tiffanykyi@gmail.com>
2018-05-02 15:40:53 -0700

    fix: Add link to documentation

--- setup.py
+++ setup.py
@@ -292,8 +292,11 @@ def main():
     A few more things to note:
       - To stop/reset Screwdriver
         $ docker-compose -p screwdriver down
+      - To rebuild Screwdriver
+        $ docker-compose build --no-cache
       - If your internal IP changes, update the docker-compose.yml and your SCM OAuth application
       - In-a-box does not support Webhooks including PullRequests for triggering builds
+      - For further documentation, see https://docs.screwdriver.cd/cluster-management/running-locally
       - For help with this and more, find us on Slack at https://slack.screwdriver.cd
 
 ❤️   Screwdriver Crew
